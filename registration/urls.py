6e79593a85ff5865808bea633aecf28f82551eb4
Shivani Chauhan <shivanichauhandev.@gmail.com>
2025-07-20 17:13:32 +0530

    add render url and postman collection in readme file

--- registration/urls.py
+++ registration/urls.py
@@ -1,14 +1,3 @@
-# from django.urls import path
-# from .views import RegisterUser,LoginUser,UserSelfView
-
-# urlpatterns = [
-#     path('register/', RegisterUser.as_view(), name='register'),
-#     path('login/', LoginUser.as_view(), name='login'), 
-#     # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-#     path('me/', UserSelfView.as_view(), name='user-self'),
-# ]
-
-
 from django.urls import path
 from .views import RegisterUser, LoginUser, UserSelfView
 
@@ -17,3 +6,73 @@ urlpatterns = [
     path('login/', LoginUser.as_view(), name='login'),
     path('me/', UserSelfView.as_view(), name='user-self'),
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
